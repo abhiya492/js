bb1bc597c355aa77552e913849067f632fcbee64
Abhishek Singh <117256099+abhiya492@users.noreply.github.com>
2023-12-15 09:16:00 +0000

    conversion of DataTypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accId=1747
+let accEmail="abhi@gmail.com"
+var password = "1234556"
+accCity ="jaipur"
+
+
+//accId =2
+accEmail="rahul@gmail.com"
+accCity="up"
+password="625623"
+ /* prefer not to use var becase of issuse in block scope and functional scope */
+
+console.log(accId);
+console.table([accId, accEmail ,password , accCity])
---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,10 @@
+let score= "34"
+
+console.log(typeof(score))
+console.log(typeof score)
+
+let valueNum = Number(score)
+console.log(typeof valueNum)
+console.log(valueNum)
+
+
